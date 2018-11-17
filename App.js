44b33dde109b16e2def2b6015915eb01b01b0ab8
harlesbayu <bayuanggaraharles@gmail.com>
2018-11-17 17:00:38 +0700

    particles

--- App.js
+++ App.js
@@ -3,21 +3,12 @@ import { Provider } from 'react-redux'
 import store from './src/store'
 import {
   AsyncStorage,
-  AppRegistry,
-  Text,
   View,
   StyleSheet,
-  PixelRatio,
-  TouchableHighlight,
 } from 'react-native';
 
 import { createNavigationContainer , createTabNavigator ,createStackNavigator, SwitchNavigator } from 'react-navigation';
 
-
-import {
-  ViroVRSceneNavigator,
-  ViroARSceneNavigator,
-} from 'react-viro';
 
 import Config from './config';
 
@@ -26,7 +17,7 @@ import Register from './src/pages/Register'
 import Lobby from './src/pages/Lobby'
 import CharOption from './src/pages/CharOptions'
 import LoadingPreGame from './src/pages/LoadingPreGame'
-import Barrack from './src/pages/Barrack';
+// import Barrack from './src/pages/Barrack';
 import Game from './src/pages/Game'
 import Home from './src/pages/Home'
 
@@ -110,114 +101,21 @@ var sharedProps = {
   apiKey: Config.API_KEY_VIRO,
 }
 
-// Sets the default scene you want for AR and VR
-var InitialARScene = require('./js/HelloWorldSceneAR');
-var InitialVRScene = require('./js/HelloWorldScene');
-
-var UNSET = "UNSET";
-var VR_NAVIGATOR_TYPE = "VR";
-var AR_NAVIGATOR_TYPE = "AR";
-
-// This determines which type of experience to launch in, or UNSET, if the user should
-// be presented with a choice of AR or VR. By default, we offer the user a choice.
-var defaultNavigatorType = UNSET;
-
 export default class ViroSample extends Component {
   constructor() {
     super();
 
-    this.state = {
-      navigatorType : defaultNavigatorType,
-      sharedProps : sharedProps
-    }
-    this._getExperienceSelector = this._getExperienceSelector.bind(this);
-    this._getARNavigator = this._getARNavigator.bind(this);
-    this._getVRNavigator = this._getVRNavigator.bind(this);
-    this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(this);
-    this._exitViro = this._exitViro.bind(this);
+    this.state = {}
   }
 
-  // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
-  // if you are building a specific type of experience.
   render() {
-
     return (
       <Provider store={store} style={styles.Container}>
         <RouteAuth/>
       </Provider>
     )
-
-    // if (this.state.navigatorType == UNSET) {
-    //   return this._getExperienceSelector();
-    // } 
-    // else if (this.state.navigatorType == VR_NAVIGATOR_TYPE) {
-    //   return this._getVRNavigator();
-    // } 
-    // else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
-    //   return this._getARNavigator();
-    // }
   }
 
-  // Presents the user with a choice of an AR or VR experience
-  _getExperienceSelector() {
-    return (
-      <View style={localStyles.outer} >
-        <View style={localStyles.inner} >
-
-          <Text style={localStyles.titleText}>
-            Choose your desired experience:
-          </Text>
-
-          <TouchableHighlight style={localStyles.buttons}
-            onPress={this._getExperienceButtonOnPress(AR_NAVIGATOR_TYPE)}
-            underlayColor={'#68a0ff'} >
-
-            <Text style={localStyles.buttonText}>AR</Text>
-          </TouchableHighlight>
-
-          <TouchableHighlight style={localStyles.buttons}
-            onPress={this._getExperienceButtonOnPress(VR_NAVIGATOR_TYPE)}
-            underlayColor={'#68a0ff'} >
-
-            <Text style={localStyles.buttonText}>VR</Text>
-          </TouchableHighlight>
-        </View>
-      </View>
-    );
-  }
-
-  // Returns the ViroARSceneNavigator which will start the AR experience
-  _getARNavigator() {
-    return (
-      <ViroARSceneNavigator {...this.state.sharedProps}
-        initialScene={{scene: InitialARScene}} />
-    );
-  }
-  
-  // Returns the ViroSceneNavigator which will start the VR experience
-  _getVRNavigator() {
-    return (
-      <ViroVRSceneNavigator {...this.state.sharedProps}
-        initialScene={{scene: InitialVRScene}} onExitViro={this._exitViro}/>
-    );
-  }
-
-  // This function returns an anonymous/lambda function to be used
-  // by the experience selector buttons
-  _getExperienceButtonOnPress(navigatorType) {
-    return () => {
-      this.setState({
-        navigatorType : navigatorType
-      })
-    }
-  }
-
-  // This function "exits" Viro by setting the navigatorType to UNSET.
-  _exitViro() {
-    this.setState({
-      navigatorType : UNSET
-    })
-  }
 }
 
 const styles = {
